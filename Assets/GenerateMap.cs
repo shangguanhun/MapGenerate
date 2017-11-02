ac5f8ae48fe71765fda08a5e08719cd01ef952ff
shangguanhun <10762700783@qq.com>
2017-11-02 16:49:31 +0800

    构建异步地图生成流程，完成从像素获取到border点的保存

--- Assets/GenerateMap.cs
+++ Assets/GenerateMap.cs
@@ -1,16 +1,101 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GenerateMap : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Color borderColor = Color.white;
+    public Color changeColor = Color.red;
+    public int mapSizeX;
+    public int mapSizeY;
+    public Texture2D map = null;
+    public RawImage image;
+    public int cityNum = 0;
+
+    public List<Vector2> pointList = new List<Vector2>();
+    public List<Vector2> borderPointList = new List<Vector2>();
+
+    void Start () {
+
+        map = Resources.Load("map") as Texture2D;
+        map=GameObject.Instantiate(map);
+        image.texture = map;
+        image.SetNativeSize();
+        mapSizeX = map.width;
+        mapSizeY = map.height;
+        StartCoroutine(GetCitys());
+    }
+
+    void SetPixel(int x,int y)
+    {
+        map.SetPixel(x, y, Color.red);
+        map.Apply();
+    }
+
+    void TryAddPoint(int x,int y)
+    {
+        if(map.GetPixel(x,y) != changeColor)
+        {
+            pointList.Add(new Vector2(x, y));
+        }
+    }
+
+    void TrySetPixel(int x,int y)
+    {
+        if (map.GetPixel(x, y) != changeColor)
+        {
+            SetPixel(x, y);
+            TryAddPoint(x - 1, y);
+            TryAddPoint(x + 1, y);
+            TryAddPoint(x, y + 1);
+            TryAddPoint(x, y - 1);
+        }
+        else
+        {
+            borderPointList.Add(new Vector2(x, y));
+        }
+    }
+
+    IEnumerator GetCityFromPoint(int x, int y)
+    {
+        pointList.Add(new Vector2(x, y));
+        borderPointList.Clear();
+        while (pointList.Count > 0)
+        {
+            Vector2 point = pointList[0];
+            pointList.RemoveAt(0);
+            TrySetPixel((int)point.x, (int)point.y);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    IEnumerator makeCityMesh(int x,int y)
+    {
+        Mesh mesh = new Mesh();
+        foreach (Vector2 vec in borderPointList)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    IEnumerator GetCitys()
+    {
+        for(int i = 0; i < mapSizeX; i+=10)
+        {
+            for (int j = 0; j < mapSizeY; j += 10)
+            {
+                if (map.GetPixel(i, j) != changeColor && map.GetPixel(i, j) != borderColor)
+                {
+                    StartCoroutine(GetCityFromPoint(i, j));
+                    yield return new WaitForSeconds(5);
+                    if (borderPointList.Count > 0)
+                    {
+                        StartCoroutine(makeCityMesh(i, j));
+                        yield return new WaitForSeconds(5);
+                    }
+                }
+            }
+        }
+    }
 }
