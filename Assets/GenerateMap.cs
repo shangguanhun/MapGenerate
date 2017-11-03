8f9b9d9940f1dcc78724fc8c7b932c513d86d25f
shangguanhun <10762700783@qq.com>
2017-11-03 11:19:58 +0800

    ps处理图片，完成图片的实时绘制及填充效果展示，加速代码执行速度

--- Assets/GenerateMap.cs
+++ Assets/GenerateMap.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 
 public class GenerateMap : MonoBehaviour {
 
-    public Color borderColor = Color.white;
-    public Color changeColor = Color.red;
+    public Color mapColor = Color.red;
+    public Color changeColor = Color.grey;
     public int mapSizeX;
     public int mapSizeY;
     public Texture2D map = null;
@@ -32,13 +32,12 @@ public class GenerateMap : MonoBehaviour {
 
     void SetPixel(int x,int y)
     {
-        map.SetPixel(x, y, Color.red);
-        map.Apply();
+        map.SetPixel(x, y, changeColor);
     }
 
-    void TryAddPoint(int x,int y)
+    void TryAddPoint(int x, int y)
     {
-        if(map.GetPixel(x,y) != changeColor)
+        if (map.GetPixel(x, y) == mapColor)
         {
             pointList.Add(new Vector2(x, y));
         }
@@ -46,7 +45,7 @@ public class GenerateMap : MonoBehaviour {
 
     void TrySetPixel(int x,int y)
     {
-        if (map.GetPixel(x, y) != changeColor)
+        if (map.GetPixel(x, y) == mapColor)
         {
             SetPixel(x, y);
             TryAddPoint(x - 1, y);
@@ -64,12 +63,19 @@ public class GenerateMap : MonoBehaviour {
     {
         pointList.Add(new Vector2(x, y));
         borderPointList.Clear();
+        int timer = 0;
         while (pointList.Count > 0)
         {
             Vector2 point = pointList[0];
-            pointList.RemoveAt(0);
             TrySetPixel((int)point.x, (int)point.y);
-            yield return new WaitForFixedUpdate();
+            pointList.RemoveAt(0);
+            if (timer > 100)
+            {
+                timer = 0;
+                map.Apply();
+                yield return new WaitForFixedUpdate();
+            }
+            timer++;
         }
         isGetPointOver = true;
     }
@@ -77,20 +83,52 @@ public class GenerateMap : MonoBehaviour {
     IEnumerator makeCityMesh(int x,int y)
     {
         Mesh mesh = new Mesh();
+        Vector3[] m_vertices;
+        Vector2[] m_uv;
+        int[] m_triangles;
+        //Color[] m_color;
+        //Vector3[] m_normals;
+        m_vertices = new Vector3[borderPointList.Count+1];
+        m_uv = new Vector2[borderPointList.Count * 2];
+        m_triangles = new int[borderPointList.Count*3];
+
+        m_vertices[0] = new Vector3(x, y, 0);
+
+        yield return new WaitForFixedUpdate();
+
+        int i = 0;
         foreach (Vector2 vec in borderPointList)
         {
-            yield return new WaitForFixedUpdate();
+            m_vertices[i + 1] = new Vector3(vec.x, vec.y, 0);
+            m_uv[i * 2] = Vector2.zero;
+            m_uv[i * 2 + 1] = new Vector2(1, i / borderPointList.Count);
+            m_triangles[i * 3] = 0;
+            m_triangles[i * 3 + 1] = i + 1;
+            m_triangles[i * 3 + 2] = i + 2;
+            if (i == borderPointList.Count)
+                m_triangles[i * 3 + 2] = 0;
+            i++;
+            if(i%100==0)
+                yield return new WaitForFixedUpdate();
         }
+
+        mesh.Clear();
+        mesh.vertices = m_vertices;
+        mesh.uv = m_uv;
+        mesh.triangles = m_triangles;
+        //mesh.colors = m_color;
+        //mesh.normals = m_normals;
+        mesh.RecalculateNormals();
         isMakeMeshOver = true;
     }
-
+    
     IEnumerator GetCitys()
     {
-        for(int i = 0; i < mapSizeX; i+=10)
+        for(int i = 0; i < mapSizeX; i+=16)
         {
-            for (int j = 0; j < mapSizeY; j += 10)
+            for (int j = 0; j < mapSizeY; j += 16)
             {
-                if (map.GetPixel(i, j) != changeColor && map.GetPixel(i, j) != borderColor)
+                if (map.GetPixel(i, j) == mapColor)
                 {
                     isGetPointOver = false;
                     isMakeMeshOver = false;
